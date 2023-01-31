aa37a849cf70b7fcdcd4aa34474132ea2d4f3bc6
Rahul Sharma <rahulpancholi3937@gmail.com>
2023-01-31 19:51:35 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 // import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies,setMovies]=useState([])
+  const [input,setInput]=useState("")
+
 
 
   const data = async ()=>{
+    
 
     await axios ({
       method: 'GET',
-      url: 'http://www.omdbapi.com/?apikey=d67d903a&t=',
+      url: `http://www.omdbapi.com/?apikey=d67d903a&t=${input}`,
       
       
     }).then((res)=>{
       console.log(res.data)
-
+     
+      setMovies(res.data)
     })
 
     
   }
-
+const search =()=>{
+  data()
+}
   useEffect (()=>{
     data()
 
@@ -28,6 +35,16 @@ function App() {
 
   return (
     <div className="App">
+       <input type="text" value={input} onChange = {(e)=>setInput(e.target.value)} />
+      <input type="submit"  onClick={search}/>
+
+
+
+      <img src={movies.Poster} alt="" />
+     
+      <h1>{movies.Title}</h1>
+     
+      
    
     </div>
   );
